b3a8565a9d8011207d559ff4cec5e394a32dc117
Chiara Cusi <cusichia@gmail.com>
2025-06-15 11:07:30 +0200

    ejercicios componentes - link.jsx

---
+++ ejercicios-componentes/src/App.jsx
@@ -0,0 +1,19 @@
+import './/styles/App.css';
+import './/styles/index.css';
+import React from 'react';
+import Header from './components/Header.jsx';
+import Main from './components/Main.jsx';
+
+function App() {
+  
+
+  return (
+    <>
+    <Header></Header>
+    <Main></Main>
+      
+    </>
+  )
+}
+
+export default App
---
+++ ejercicios-componentes/src/components/Header.jsx
@@ -0,0 +1,24 @@
+function Header() {
+    return (
+      <>
+        <h1>Header</h1>
+        
+        <nav>
+          <ul>
+            <li class="menu-item">
+              <a title="Blog" class="menu-link" href="https://adalab.es/blog/">
+                Blog
+              </a>
+            </li>
+            <li class="menu-item">
+              <a title="Contacto" class="menu-link" href="https://adalab.es/contacto/">
+                Contacto
+              </a>
+            </li>
+          </ul>
+      </nav>
+
+      </>
+    );
+  }
+  export default Header;
